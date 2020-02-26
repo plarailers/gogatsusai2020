23bb46739961c8718ee0a611b3547ad61b43c4bb
Nitto-R <nitryo.run0524@gmail.com>
2020-02-26 11:13:34 +0900

    update

--- hole_sensor/hole_sensor.ino
+++ hole_sensor/hole_sensor.ino
@@ -7,7 +7,7 @@ int input = 150;//モーターへの初期入力(0~255)
 double speed_id = 30;//車両の速度目標値(cm/s)
 double kp = 3;//比例係数
 double kd = 3;//微分係数
-double ki = 0.1;//積分係数
+double ki = 0.01;//積分係数
 /*------------------------------------------*/
 
 const int SENSOR_PIN = 0;
@@ -33,15 +33,18 @@ void loop(){
   digitalWrite(INPUT_PIN, input);
   int value = analogRead(SENSOR_PIN);
 
+  //磁石がホールセンサーの上にきたら
   if (status = 0 && value >= 512) {
     status = 1;
     period = new_time - old_time;
     old_time = new_time;
+    //ここにPCに発信する命令を書く
   }
   else if (status = 1 && value < 512) {
     status = 0;
   }
 
+  //周期periodを速度speedに変換
   speed = 2000*3.1415926535*r/(double)period;
   e2 = e1;
   e1 = e0;
@@ -50,7 +53,7 @@ void loop(){
   if (input > 255) {
     input = 255;
   }
-  else if (input <0) {
+  else if (input < 0) {
     input = 0;
   }
 
