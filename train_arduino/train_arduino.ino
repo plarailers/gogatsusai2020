76b0478aebe29e0ff0e309eefa09758f91d3102b
Nitto-R <nitryo.run0524@gmail.com>
2020-02-27 13:34:04 +0900

    update

--- train_arduino/train_arduino.ino
+++ train_arduino/train_arduino.ino
@@ -1,15 +1,13 @@
-#include <Servo.h>
-
 /*調整する変数--------------------------------*/
-int input = 150;//モーターへの初期入力(0~255)
-double speed_id = 30;//車両の速度目標値(cm/s)
-double kp = 3;//比例係数
-double kd = 3;//微分係数
-double ki = 0.01;//積分係数
+int input = 130;//モーターへの初期入力(0~255)
+double speed_id = 40;//車両の速度目標値(cm/s)
+double kp = 1;//比例係数
+double kd = 1;//微分係数
+double ki = 0.001;//積分係数
 /*------------------------------------------*/
 
 const int SENSOR_PIN = 0;
-const int INPUT_PIN = 8;
+const int INPUT_PIN = 6;
 double speed = 0;//車両の速度
 bool status = 0;
 unsigned int new_time = 0;
@@ -28,32 +26,40 @@ void setup() {
 void loop(){
   
   new_time = millis();
-  digitalWrite(INPUT_PIN, input);
+  analogWrite(INPUT_PIN, input);
   int value = analogRead(SENSOR_PIN);
+  Serial.print(value);
 
   //磁石がホールセンサーの上にきたら
-  if (status = 0 && value >= 512) {
+  if (status == 0 && value >= 512) {
     status = 1;
     period = new_time - old_time;
+    Serial.print(" ");
+    Serial.print(period);
     old_time = new_time;
+    
+    //周期periodを速度speedに変換
+    speed = 2000*3.1415926535*r/(double)period;
+    e2 = e1;
+    e1 = e0;
+    e0 = speed_id - speed;
+    input += (int)(kp*e0 + kd*(e0-e1) + ki*(e0+e1+e2));
+    if (input > 255) {
+      input = 255;
+    }
+    else if (input < 80) {
+      input = 80;
+    } 
     //ここにPCに発信する命令を書く
   }
-  else if (status = 1 && value < 512) {
+  else if (status == 1 && value < 512) {
     status = 0;
   }
-
-  //周期periodを速度speedに変換
-  speed = 2000*3.1415926535*r/(double)period;
-  e2 = e1;
-  e1 = e0;
-  e0 = speed_id - speed;
-  input += (int)(kp*e0 + kd*(e0-e1) + ki*(e0+e1+e2));
-  if (input > 255) {
-    input = 255;
-  }
-  else if (input < 0) {
-    input = 0;
-  }
+  
+  Serial.print(" ");
+  Serial.print(speed);
+  Serial.print(" ");
+  Serial.println(input);
 
 }
   
