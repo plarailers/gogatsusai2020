7543ac82fa1df7eaf4fa0aa6415f4d08675b4213
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-08-02 16:39:48 +0900

    エラーコードの追加

--- backend/src/handler.ts
+++ backend/src/handler.ts
@@ -2,6 +2,29 @@ import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } f
 import { ApiGatewayManagementApi, DynamoDB } from 'aws-sdk';
 import 'source-map-support/register';
 
+const ERROR = {
+  INVALID_JSON: {
+    code: 1,
+    message: `無効なJSON形式です。`,
+  },
+  PASSWORD_NOT_SET: {
+    code: 2,
+    message: `パスワードが必要です。`,
+  },
+  PASSWORD_NOT_EXIST: {
+    code: 3,
+    message: `パスワードが存在しません。`,
+  },
+  PASSWORD_NOT_YET_VALID: {
+    code: 4,
+    message: `パスワードの有効期限が始まっていません。`,
+  },
+  PASSWORD_EXPIRED: {
+    code: 5,
+    message: `パスワードの有効期限が切れています。`,
+  },
+};
+
 const apigateway = ({ domainName, stage }): ApiGatewayManagementApi => new ApiGatewayManagementApi({ endpoint: `${domainName}/${stage}` });
 const dynamodb = new DynamoDB.DocumentClient();
 
@@ -37,34 +60,45 @@ export const disconnect: APIGatewayProxyHandler = async (event: APIGatewayProxyE
   };
 };
 
+async function checkPassword(body: string) {
+  let parsed = null;
+  try {
+    parsed = JSON.parse(body);
+  } catch (e) {
+    return ERROR.INVALID_JSON;
+  }
+  if (typeof parsed !== "object") {
+    return ERROR.INVALID_JSON;
+  }
+  const password = parsed.password;
+  if (!password) {
+    return ERROR.PASSWORD_NOT_SET;
+  }
+  const { Item } = await dynamodb.get({ TableName: process.env.DYNAMODB_PASSWORDS, Key: { Password: password } }).promise();
+  if (!Item) {
+    return ERROR.PASSWORD_NOT_EXIST;
+  }
+  const startTime = new Date(Item.StartTime).getTime();
+  const endTime = new Date(Item.EndTime).getTime();
+  const now = Date.now();
+  if (now < startTime) {
+    return ERROR.PASSWORD_NOT_YET_VALID;
+  } else if (endTime < now) {
+    return ERROR.PASSWORD_EXPIRED;
+  }
+  return null;
+}
+
 export const defaultMessage: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.debug('Starting Lambda handler: event=%s', JSON.stringify(event));
-  try {
-    const parsed = JSON.parse(event.body);
-    const password = parsed.password;
-    if (password === undefined) {
-      throw new Error('パスワードが必要です。');
-    }
-    const result = await dynamodb.get({ TableName: process.env.DYNAMODB_PASSWORDS, Key: { Password: password } }).promise();
-    if (result.Item) {
-      const { Item } = result;
-      const startTime = new Date(Item.StartTime).getTime();
-      const endTime = new Date(Item.EndTime).getTime();
-      const now = Date.now();
-      if (now < startTime) {
-        throw new Error('パスワードの有効期限が始まっていません。');
-      } else if (endTime < now) {
-        throw new Error('パスワードの有効期限が切れています。');
-      }
-    } else {
-      throw new Error('パスワードが存在しません。');
-    }
-  } catch (e) {
+  const passwordError = await checkPassword(event.body);
+  if (passwordError) {
     const api = new ApiGatewayManagementApi({ endpoint: `${event.requestContext.domainName}/${event.requestContext.stage}` });
     await api.postToConnection({
       ConnectionId: event.requestContext.connectionId,
       Data: JSON.stringify({
-        message: `${e}`,
+        code: passwordError.code,
+        message: passwordError.message,
       }),
     }).promise();
     return {
@@ -72,12 +106,16 @@ export const defaultMessage: APIGatewayProxyHandler = async (event: APIGatewayPr
       body: JSON.stringify({}),
     };
   }
+  const parsed = JSON.parse(event.body);
   const connections = await dynamodb.scan({ TableName: process.env.DYNAMODB_CONNECTIONS, ProjectionExpression: 'ConnectionId' }).promise();
   await Promise.all(connections.Items.map(async ({ ConnectionId }) => {
     try {
       const params: ApiGatewayManagementApi.Types.PostToConnectionRequest = {
         ConnectionId: ConnectionId,
-        Data: event.body,
+        Data: JSON.stringify({
+          code: 0,
+          ...parsed,
+        }),
       };
       const { domainName, stage } = event.requestContext;
       await apigateway({domainName, stage}).postToConnection(params).promise();
